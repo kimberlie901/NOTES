deb0d1b13e964f9165b0c57367a4fb8dd89a06f0
Kimmy <kimberlieguillaume@gmail.com>
2023-04-09 13:38:31 -0400

    debugging

--- Routes/routesAPI.js
+++ Routes/routesAPI.js
@@ -1,18 +1,26 @@
 //Dependencies 
 const router = require("express").Router();
+const { json, response } = require("express");
 const notes = require("../db/db.json");
 const fs = require("fs");
+const path = require("path");
 //Gives each code a unique id when it's saved 
 const {v4: uuidv4} = require("uuid");
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON
+// router.get("/notes", (req, res) => {
+//    res.json(notes);
+// });
+
 router.get("/notes", (req, res) => {
-   res.json(notes);
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf-8", (error, data) => {
+        if(error) return res.json(error);
+        data ? res.json(JSON.parse(data)) : res.json(false);
+    });
 });
 
 
 //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique ID when it's saved (look into npm packages that could do this for you).
-
 router.post("/notes", (req, res) => {
     let newNote = req.body;
     notes.push(newNote);
--- Routes/routesHTML.js
+++ Routes/routesHTML.js
@@ -16,7 +16,7 @@ router.get("/notes", (req, res) => {
 // Get * this will return index.html file as a response to the user when the GET request is made
 
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router; 
