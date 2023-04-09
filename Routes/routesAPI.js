696ba47de2c65f5b326c9df47477cd0fd5f7ed33
Kimmy <kimberlieguillaume@gmail.com>
2023-04-08 20:46:47 -0400

    debugging

--- Routes/routesAPI.js
+++ Routes/routesAPI.js
@@ -1,11 +1,13 @@
 //Dependencies 
 const router = require("express").Router();
 const notes = require("../db/db.json");
+const fs = require("fs");
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON
 
 router.get("/notes", (req, res) => {
-    notes.getNotes()
+    notes.getNotes();
+    fs.readFile("./db/db.json", "utf-8", (err, data))
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
@@ -13,7 +15,8 @@ router.get("/notes", (req, res) => {
 //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique ID when it's saved (look into npm packages that could do this for you).
 
 router.post("/notes", (req, res) => {
-    notes.addNotes()
+    notes.addNotes(req.body);
+    fs.writeFileSync("./db/db.json", "utf-8", (err, data))
     .then(note => res.json(note))
     .catch(err => res.status(500).json(err));
 });
